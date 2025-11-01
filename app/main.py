137389e0d7c7fa6189f9192f3abe2f4a6386cdd5
Kyrylo Kilin <killin@ukr.net>
2025-11-01 11:29:08 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
-# write your code here
+def copy_file(command: str) -> None:
+
+    parts = command.strip().split()
+    if len(parts) != 3:
+        return
+
+    cmd, src, dst = parts
+    if cmd != "cp" or src == dst:
+        return
+
+    try:
+        with open(src, "rb") as fin, open(dst, "wb") as fout:
+            while True:
+                chunk = fin.read(1024 * 1024)
+                if not chunk:
+                    break
+                fout.write(chunk)
+    except FileNotFoundError:
+        return
